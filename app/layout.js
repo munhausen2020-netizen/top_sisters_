3931b34e681761721c9c98698635e815e3aed9c8
Imran72 <munhausen2020@gmail.com>
2026-09-20 21:33:15 +0300

    asd

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,25 @@
 import "./globals.css";
+import { Press_Start_2P } from "next/font/google";
+
+const pressStart2P = Press_Start_2P({
+  weight: "400",
+  subsets: ["latin", "cyrillic"],
+  variable: "--font-pixel",
+  display: "swap",
+});
 
 export const metadata = {
   title: "WOMEN NAME",
-  description: "Рейтинг женских имён. Голосуй за своё имя и поднимай его выше.",
+  description:
+      "Рейтинг женских имён. Голосуй за своё имя и поднимай его выше.",
 };
 
 export default function RootLayout({ children }) {
   return (
       <html lang="ru">
-      <body>{children}</body>
+      <body className={pressStart2P.variable}>
+      {children}
+      </body>
       </html>
   );
 }
