7bf9d7458314b10017bd4122af8ef59a8eaff7ac
Imran72 <munhausen2020@gmail.com>
2026-09-22 13:31:45 +0300

    asdasd

--- app/api/payments/create/route.js
+++ app/api/payments/create/route.js
@@ -125,7 +125,7 @@ export async function POST(request) {
 
         const siteUrl =
             process.env.NEXT_PUBLIC_SITE_URL ||
-            "https://womenname.ru";
+            "https://proactive-expression-production-a967.up.railway.app";
 
 
         const response =
